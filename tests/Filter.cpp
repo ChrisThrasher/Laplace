5c5fea4873936d1f8e9c7f096121b7faf7b29730
Chris Thrasher <chrisjthrasher@gmail.com>
2023-10-06 00:22:44 -0600

    Take two removing MinGW
    I'm tired of this, Grandpa...

--- tests/Filter.cpp
+++ tests/Filter.cpp
@@ -7,7 +7,7 @@ TEMPLATE_TEST_CASE("lp::Filter", "", float, double, long double)
     SECTION("Type traits")
     {
         STATIC_CHECK(std::has_virtual_destructor_v<lp::Filter<TestType>>);
-        // STATIC_CHECK(!std::is_copy_constructible_v<lp::Filter<TestType>>); MinGW thinks this type trait is true
+        STATIC_CHECK(!std::is_copy_constructible_v<lp::Filter<TestType>>);
         STATIC_CHECK(std::is_copy_assignable_v<lp::Filter<TestType>>);
         STATIC_CHECK(!std::is_nothrow_move_constructible_v<lp::Filter<TestType>>);
         STATIC_CHECK(std::is_nothrow_move_assignable_v<lp::Filter<TestType>>);
