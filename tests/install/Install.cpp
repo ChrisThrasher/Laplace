60d5b367cc8d2b9eca446f0cee80f13a594f7699
Chris Thrasher <chrisjthrasher@gmail.com>
2023-10-05 23:39:11 -0600

    Improve install test

--- tests/install/Install.cpp
+++ tests/install/Install.cpp
@@ -1,3 +1,14 @@
+#include <Laplace/Angle.hpp>
+#include <Laplace/Fft.hpp>
+#include <Laplace/Filter.hpp>
+#include <Laplace/Frequency.hpp>
+#include <Laplace/HighPass.hpp>
 #include <Laplace/LowPass.hpp>
+#include <Laplace/Rng.hpp>
 
-int main() { [[maybe_unused]] auto filter = lp::LowPass(lp::hertz(10.), 0.); }
+int main()
+{
+    [[maybe_unused]] auto rng = lp::rng();
+    [[maybe_unused]] auto lpf = lp::LowPass(lp::hertz(10.), 0.);
+    [[maybe_unused]] auto hpf = lp::HighPass(lp::hertz(10.), 0.);
+}
