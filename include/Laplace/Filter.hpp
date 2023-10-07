2bec5a9e1d3e4a5bb9a28dae194caa0163913f85
Chris Thrasher <chrisjthrasher@gmail.com>
2023-10-06 22:13:56 -0600

    Enable rule-of-5 check

--- include/Laplace/Filter.hpp
+++ include/Laplace/Filter.hpp
@@ -10,7 +10,12 @@ class Filter {
 public:
     static_assert(std::is_floating_point_v<ValueType>, "ValueType must be floating point");
 
+    Filter() = default;
     virtual ~Filter() = default;
+    Filter(const Filter&) = default;
+    Filter& operator=(const Filter&) = default;
+    Filter(Filter&&) noexcept = default;
+    Filter& operator=(Filter&&) noexcept = default;
 
     virtual ValueType operator()(ValueType input, std::chrono::nanoseconds dt) = 0;
     [[nodiscard]] virtual ValueType value() const = 0;
