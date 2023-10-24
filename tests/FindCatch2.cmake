1a18e465615ded04a06dc006a3533df69dc78f98
Chris Thrasher <chrisjthrasher@gmail.com>
2023-10-24 13:49:45 -0500

    Modify fewer Catch2 properties

--- tests/FindCatch2.cmake
+++ tests/FindCatch2.cmake
@@ -6,5 +6,5 @@ FetchContent_Declare(Catch2
     GIT_SHALLOW ON
     SYSTEM)
 FetchContent_MakeAvailable(Catch2)
-set_target_properties(Catch2 PROPERTIES COMPILE_OPTIONS "" EXPORT_COMPILE_COMMANDS OFF)
+set_target_properties(Catch2 PROPERTIES EXPORT_COMPILE_COMMANDS OFF)
 set_target_properties(Catch2WithMain PROPERTIES EXPORT_COMPILE_COMMANDS OFF)
